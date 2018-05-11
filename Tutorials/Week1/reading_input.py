270a582d462818a3c5589a35aeebbe879ea974d3
Sandeep Sadarangani <sandeep.97@outlook.com>
2018-05-11 18:56:58 +1000

    completed program: reading_input.py

---
+++ Tutorials/Week1/reading_input.py
@@ -0,0 +1,8 @@
+# Week 1 - Tutorial 1: Introduction, Terminal and Output
+# Info1110 - Sandeep S
+# 11/05/2018
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+print("Your name is " + first_name + last_name)
