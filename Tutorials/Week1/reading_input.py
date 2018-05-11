229c8dfb0ddef88344b79ffd4a43cb42bba72f18
Sandeep Sadarangani <sandeep.97@outlook.com>
2018-05-11 19:57:08 +1000

    Completed week 1 programs

--- Tutorials/Week1/reading_input.py
+++ Tutorials/Week1/reading_input.py
@@ -1,6 +1,8 @@
 # Week 1 - Tutorial 1: Introduction, Terminal and Output
 # Info1110 - Sandeep S
 # 11/05/2018
+# reading_input.py - Ask the user for their name from std input and print
+# to std output
 
 first_name = input("What is your first name? ")
 last_name = input("What is your last name? ")
